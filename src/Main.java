5e3f13e8149eddd7334ed08e476abbd28ad93a73
oleg- <o.boys@yandex,ru>
2022-08-19 16:58:21 +0300

    5 Задание

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int monthNumber = 12;
+        switch(monthNumber)
+        {
+            case 1:
+                System.out.println("Winter!");
+                break;
+            case 2:
+                System.out.println("Winter!");
+                break;
+            case 3:
+                System.out.println("Spring!");
+                break;
+            case 4:
+                System.out.println("Spring!");
+                break;
+            case 5:
+                System.out.println("Spring!");
+                break;
+            case 6:
+                System.out.println("Summer!");
+                break;
+            case 7:
+                System.out.println("Summer!");
+                break;
+            case 8:
+                System.out.println("Summer!");
+                break;
+            case 9:
+                System.out.println("Autumn!");
+                break;
+            case 10:
+                System.out.println("Autumn!");
+                break;
+            case 11:
+                System.out.println("Autumn!");
+            case 12:
+                System.out.println("Winter!");
+                break;
+            default:
+                System.out.println("Всего 12 месяцев!");
+                break;
+
+
+        }
+    }
 }
